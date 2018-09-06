ee4e1b1264107ee85de2337301af7a62365190ff
Jose Herrera <31715468+joseherrera7@users.noreply.github.com>
2018-09-06 07:47:53 -0600

    First Commit

---
+++ app/src/main/java/com/ed2/joseherrera/lab1_ed2/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ed2.joseherrera.lab1_ed2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
