1359182e69f3986db25c97abd36122e30a06e868
Jose Herrera <31715468+joseherrera7@users.noreply.github.com>
2018-09-15 20:38:26 -0600

    Leer archivo ready

--- app/src/main/java/com/ed2/joseherrera/lab1_ed2/MainActivity.java
+++ app/src/main/java/com/ed2/joseherrera/lab1_ed2/MainActivity.java
@@ -1,93 +1,132 @@
 package com.ed2.joseherrera.lab1_ed2;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.ed2.joseherrera.lab1_ed2.Huffman.Huffman;
 import com.ed2.joseherrera.lab1_ed2.Huffman.Nodo;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.PriorityQueue;
 import java.util.Scanner;
+import android.view.View.OnClickListener;
 import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
     static int ASCII[] = new int[128];
+    private Button btnCargarArchivo;
+    String entry;
+    String prueba="";
+    String nArchivo = "data.txt";
+    Context ctx = this;
+    FileOutputStream fos;
+    FileInputStream fis;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        String FILENAME = "data.txt";
+        String string = "Llueve en este poema\n" +
+                "Eduardo Carranza.\n" +
+                "Llueve. La tarde es una\n" +
+                "hoja de niebla. Llueve.\n" +
+                "La tarde está mojada\n" +
+                "de tu misma tristeza.\n" +
+                "A veces viene el aire\n" +
+                "con su canción. A veces\n" +
+                "Siento el alma apretada\n" +
+                "contra tu voz ausente.\n" +
+                "Llueve. Y estoy pensando\n" +
+                "en ti. Y estoy soñando.\n" +
+                "Nadie vendrá esta tarde\n" +
+                "a mi dolor cerrado.\n" +
+                "Nadie. Solo tu ausencia\n" +
+                "que me duele en las horas.\n" +
+                "Mañana tu presencia regresará en la rosa.\n" +
+                "Yo pienso cae la lluvia\n" +
+                "nunca como las frutas.\n" +
+                "Niña como las frutas,\n" +
+                "grata como una fiesta\n" +
+                "hoy esta atardeciendo\n" +
+                "tu nombre en mi poema.\n" +
+                "A veces viene el agua\n" +
+                "a mirar la ventana\n" +
+                "Y tú no estás\n" +
+                "A veces te presiento cercana.\n" +
+                "Humildemente vuelve\n" +
+                "tu despedida triste.\n" +
+                "Humildemente y todo\n" +
+                "humilde: los jazmines\n" +
+                "los rosales del huerto\n" +
+                "y mi llanto en declive.\n" +
+                "Oh, corazón ausente:\n" +
+                "qué grande es ser humilde.";
+
+        FileOutputStream fos = null;
+        try {
+            if (openFileInput(FILENAME)!=null){
+            fos = openFileOutput(FILENAME, Context.MODE_APPEND);}
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            fos.write(string.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            fos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        btnCargarArchivo =  (Button) findViewById(R.id.btnCargarDatos);
+        btnCargarArchivo.setOnClickListener(new OnClickListener() {
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                FileManager fileManager = null;
+                entry = leerArchivo();
+                Huffman huffi=new Huffman(entry);
+                huffi.ejecutarHuffman();
+                prueba= huffi.escribirbinario(entry);
+            }
+        });
+        
         ArrayList<Nodo> letras=new ArrayList<Nodo>();
-        String entrada="hjdgashdgasdhsajg ashdjgsadhasgdjhs hgdasjhdgsajhdgsa jfdsajgdgsajhd hgsajdgdhsgd \n" +
-                "hdgsahdgasjhdgsajgdh\n" +
-                "khgshdgsahdgskdhkj jhsgdsahdg" +
-                "dhsgdjsgad" +
-                "kjhjkhjk";
+        
 
-String prueba="";
-      /*  Nodo letra1=new Nodo();
-        letra1.setPorcentaje(0.236);
-        letras.add(letra1);
 
-        Nodo letra2=new Nodo();
-        letra2.setPorcentaje(0.963);
-        letras.add(letra2);
 
-        Nodo letra3=new Nodo();
-        letra3.setPorcentaje(0.321);
-        letras.add(letra3);
 
-        Nodo letra4=new Nodo();
-        letra4.setPorcentaje(0.896);
-        letras.add(letra4);
-
-        Nodo letra5=new Nodo();
-        letra5.setPorcentaje(0.520);
-        letras.add(letra5);
-
-        Nodo letra6=new Nodo();
-        letra6.setPorcentaje(0.210);
-        letras.add(letra6);
-
-        Nodo letra7=new Nodo();
-        letra7.setPorcentaje(0.120);
-        letras.add(letra7);
-
-        Nodo letra8=new Nodo();
-        letra8.setPorcentaje(0.239);
-        letras.add(letra8);
-
-        Nodo letra9=new Nodo();
-        letra9.setPorcentaje(0.520);
-        letras.add(letra9);
-
-        Nodo letra10=new Nodo();
-        letra10.setPorcentaje(0.100);
-        letras.add(letra10);
-
-        Nodo letra11=new Nodo();
-        letra11.setPorcentaje(0.756);
-        letras.add(letra11);
-
-        Nodo letra12=new Nodo();
-        letra12.setPorcentaje(0.963);
-        letras.add(letra12);
-
-        Nodo letra13=new Nodo();
-        letra13.setPorcentaje(0.211);
-        letras.add(letra13);
-
-        Nodo letra14=new Nodo();
-        letra14.setPorcentaje(0.236);
-        letras.add(letra14);*/
-
-        Huffman huffi=new Huffman(entrada);
-        huffi.ejecutarHuffman();
-       prueba= huffi.escribirbinario(entrada);
-
+    }
+    public String leerArchivo(){
+        FileInputStream fis;
+        String content = "";
+        try {
+            fis = openFileInput(nArchivo);
+            byte[] input = new byte[fis.available()];
+            while (fis.read(input) != -1) {
+            }
+            content += new String(input);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
     }
 }
