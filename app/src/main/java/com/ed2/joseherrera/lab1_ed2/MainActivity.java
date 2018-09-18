851bb184120e88dc53700c98e000641f8c115be4
Jose Herrera <31715468+joseherrera7@users.noreply.github.com>
2018-09-18 15:34:13 -0600

    Mis compresiones y explorar de archivos v0.3

--- app/src/main/java/com/ed2/joseherrera/lab1_ed2/MainActivity.java
+++ app/src/main/java/com/ed2/joseherrera/lab1_ed2/MainActivity.java
@@ -115,10 +115,10 @@ private EditText ruta;
             @Override
             public void onClick(View view) {
 
-                entry = leerArchivo();
-                Huffman huffi=new Huffman(entry);
+
+                Huffman huffi=new Huffman(contenido.getText().toString());
                 huffi.ejecutarHuffman();
-                ecribirhuffman( huffi.escribirbinario(entry));
+                ecribirhuffman( huffi.escribirbinario(contenido.getText().toString()));
             }
         });
 
@@ -126,22 +126,8 @@ private EditText ruta;
         Descomprimir.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
-                Huffman huffi=new Huffman("a|||77|||||t|||40|||||M|||1|||||ú|||1|||||O|||1|||||é|||1|||||z|||5|||||y|||5|||||L|||6|||||c|||21|||||n|||42|||||s|||42|||||o|||42|||||m|||22|||||j|||3|||||ñ|||3|||||N|||3|||||f|||3|||||h|||7|||||p|||8|||||e|||101||||| |||111|||||d|||30|||||u|||31|||||i|||31|||||l|||32|||||á|||4|||||A|||4|||||Y|||4|||||g|||4|||||b|||2|||||ó|||2|||||S|||2|||||q|||2|||||,|||2|||||H|||2|||||:|||2|||||E|||1|||||C|||1|||||\n" +
-                        "|||34|||||r|||35|||||v|||17|||||.|||20|||||ÈC0Zv\u009DkP%Ç\u0088a9¸O(`á\u000F¼Ü\u0001d!\u0098,\u0005\u0093\u00ADá;N\u0091¯\u008A\fÃôv¬¬nN\u0002ÈC0X\u0002ÉÖð\u009D§Z>£0üüGkL¢Z\u008FZ\u0012Ö¼Ü\u0001\u000F ^\u001D Jí¦EÊ\u0016,Mª2Ç¼\u0014m`!ä\u000BÃ¡\n" +
-                        "Jïe2.Hõà\u0005ï?\u0016&ô=i\u00903\u0099sS½`\u000B!\fÁ`!Ôëg*\u0080î½\u009EDíi@C©ÖÎUS\n" +
-                        "ìò ¡yÊÐ.Î\u0011ôëzÞ\u0013±u\u0012£É$+\f!ç\u0090\u0005\u000BÎV\u0002\u001AÉ%i\u00975;Áx\u0086\u0086j-\u001CÌ\u008D;Dô È} ,ÿ\u0086÷Ö\u0099@\u000B§x/Ac\u0081uÂ>\u009D¢t\u0019® \u0087e@+º\u0095\u008EÑ:!\f\tx¬×\u008E±1\u0094OJ\u0010&ß@\n" +
-                        "\u0014°Ýbc(\u009E\u0094 M¾\u0086b\u001C\u000FÞ±1\u0094kê\u0010®·\u008A\fåN·¯¼'x+³È´Êæ#p´íDª\u0004¸ð\u0004<\u0081xt\u0081+¶\u0099\u0017\u001C7\u0017Q(xQ:\u0005Þûâ\u001D[\u009EW)Ö\u008FÐ\u0087\u0090/\u000E\u0096µ .\u008Aïea\u0087\u001Eø\u0002\u0019\u001A%!Ü]ë@\u0099\u0090\u0016-2;¨\få?Z\u0012Ö°\u000424JC¸»Ö²\u00AD\u0093È \u009A%!ØÅ\u0012iCüÑ-º\u0011&\u0090f¹\u001D#²(&aÙ\u0016U\u0012¢\u0013ÞÊv\u008EðB\u0082À\u0016v\bÍbCó\n" +
-                        "\u00ADsS½c\u0011\n" +
-                        "\n" +
-                        "\u009CC\u0081öv\u009D*aPM\u0012\u0090ì ");
-                huffi.escribirdescomp("a|||77|||||t|||40|||||M|||1|||||ú|||1|||||O|||1|||||é|||1|||||z|||5|||||y|||5|||||L|||6|||||c|||21|||||n|||42|||||s|||42|||||o|||42|||||m|||22|||||j|||3|||||ñ|||3|||||N|||3|||||f|||3|||||h|||7|||||p|||8|||||e|||101||||| |||111|||||d|||30|||||u|||31|||||i|||31|||||l|||32|||||á|||4|||||A|||4|||||Y|||4|||||g|||4|||||b|||2|||||ó|||2|||||S|||2|||||q|||2|||||,|||2|||||H|||2|||||:|||2|||||E|||1|||||C|||1|||||\n" +
-                        "|||34|||||r|||35|||||v|||17|||||.|||20|||||ÈC0Zv\u009DkP%Ç\u0088a9¸O(`á\u000F¼Ü\u0001d!\u0098,\u0005\u0093\u00ADá;N\u0091¯\u008A\fÃôv¬¬nN\u0002ÈC0X\u0002ÉÖð\u009D§Z>£0üüGkL¢Z\u008FZ\u0012Ö¼Ü\u0001\u000F ^\u001D Jí¦EÊ\u0016,Mª2Ç¼\u0014m`!ä\u000BÃ¡\n" +
-                        "Jïe2.Hõà\u0005ï?\u0016&ô=i\u00903\u0099sS½`\u000B!\fÁ`!Ôëg*\u0080î½\u009EDíi@C©ÖÎUS\n" +
-                        "ìò ¡yÊÐ.Î\u0011ôëzÞ\u0013±u\u0012£É$+\f!ç\u0090\u0005\u000BÎV\u0002\u001AÉ%i\u00975;Áx\u0086\u0086j-\u001CÌ\u008D;Dô È} ,ÿ\u0086÷Ö\u0099@\u000B§x/Ac\u0081uÂ>\u009D¢t\u0019® \u0087e@+º\u0095\u008EÑ:!\f\tx¬×\u008E±1\u0094OJ\u0010&ß@\n" +
-                        "\u0014°Ýbc(\u009E\u0094 M¾\u0086b\u001C\u000FÞ±1\u0094kê\u0010®·\u008A\fåN·¯¼'x+³È´Êæ#p´íDª\u0004¸ð\u0004<\u0081xt\u0081+¶\u0099\u0017\u001C7\u0017Q(xQ:\u0005Þûâ\u001D[\u009EW)Ö\u008FÐ\u0087\u0090/\u000E\u0096µ .\u008Aïea\u0087\u001Eø\u0002\u0019\u001A%!Ü]ë@\u0099\u0090\u0016-2;¨\få?Z\u0012Ö°\u000424JC¸»Ö²\u00AD\u0093È \u009A%!ØÅ\u0012iCüÑ-º\u0011&\u0090f¹\u001D#²(&aÙ\u0016U\u0012¢\u0013ÞÊv\u008EðB\u0082À\u0016v\bÍbCó\n" +
-                        "\u00ADsS½c\u0011\n" +
-                        "\n" +
-                        "\u009CC\u0081öv\u009D*aPM\u0012\u0090ì ");
+                Huffman huffi=new Huffman(contenido.getText().toString());
+                huffi.escribirdescomp(contenido.getText().toString());
             }
         });
        // ArrayList<Nodo> letras=new ArrayList<Nodo>();
@@ -152,24 +138,6 @@ private EditText ruta;
 
 
 
-    }
-    public String leerArchivo(){
-        FileInputStream fis;
-        String content = "";
-        try {
-            fis = openFileInput(contenido.getText().toString());
-            byte[] input = new byte[fis.available()];
-            while (fis.read(input) != -1) {
-            }
-            content += new String(input);
-        }
-        catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        catch (IOException e) {
-            e.printStackTrace();
-        }
-        return content;
     }
 
 
